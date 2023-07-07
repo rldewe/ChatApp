e4fcfe3e396ec36bca1795086859ae343ae39551
rldewe <04srishtiy@gmail.com>
2023-07-07 06:59:13 +0530

    first commit

---
+++ client/src/Routes.jsx
@@ -0,0 +1,15 @@
+import Register from "./Register";
+import {useContext} from "react";
+import {UserContext} from "./UserContext.jsx";
+import Chat from "./Chat";
+
+export default function Routes(){
+    const {username, id} = useContext(UserContext);
+
+    if (username) {
+      return <Chat />;
+    }
+    return(
+        <Register/>
+    )
+}
